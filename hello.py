c54e83047f26020937c2af8caab743fd98dad0c9
maha-khalaf <maha.khalaf@gmail.com>
2023-02-15 12:48:33 -0600

    weight/pound conversion tool

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print('hello')
+weight = input('Weight: ')
+unit = input('(L)bs or (K)g: ')
+
+if unit.toLowercase == 'l':
+    weight_in_kilo = float(weight) * 0.45
+    print('You are ' + str(weight_in_kilo) + 'kilograms')
+elif unit.toLowercase == 'k':
+    weight_in_pounds = float(weight) * 2.2
+    print('You are ' + str(weight_in_pounds) + 'pounds')
